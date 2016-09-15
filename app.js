5ef97580a313492695f869ea2b6e3b003a893f47
oleksinski <oleksii.karpynskyi@mobenga.com>
2016-09-15 13:36:27 +0300

    no message

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var exphbs  = require('express-handlebars');
 var app = express();
 
 var sports = require('./sport');
+//var data = require('./data');
 
 
 var config = {defaultLayout: 'main'};
@@ -12,7 +13,27 @@ app.set('view engine', 'handlebars');
 // TODO add cacching of tempates app.enable('view cache');
 
 app.get('/', function (req, res) {
-    res.render('home', {'sports': sports.sports()});
+    res.render('home', {
+        sports: sports.sports(),
+        tabs: [
+            {
+                id: '15min',
+                title: '15min'
+            },
+            {
+                id: 'hour',
+                title: 'Hour'
+            },
+            {
+                id: 'day',
+                title: 'Day'
+            },
+            {
+                id: 'week',
+                title: 'Week'
+            }
+        ]
+    });
 });
 
 // app.use(express.static('public'));
