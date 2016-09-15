879aeb2e1f536ec90013eb50ff237127fb1c314e
romangl <rgluschuk@gmail.com>
2016-09-15 18:18:21 +0300

    get Event by period and sport name with bets

--- utils.js
+++ utils.js
@@ -10,19 +10,53 @@ var utils = {
         return data.tabs;
     },
 
-    dataByPeriodAndSportName: function(period, sportName) {
-        var period = period;
-        var sport = data.sports.filter(function(sport){
+    getTab: function(period) {
+        return this.getTabs().filter(function(tab){
+            if(tab.id == period) {
+                return tab;
+            }
+        })[0];
+    },
+
+    getSport: function(sportName){
+       return data.sports.filter(function(sport){
             if(sport.alias == sportName) {
                 return sport;
             }
         })[0];
-        var events = data.events.filter(function(event){
+    },
+
+    getTabEvents: function(period){
+        return this.getTab(period).events;
+    },
+
+    getEventsBySport: function(sport) {
+        return data.events.filter(function(event){
             if(event.sportId == sport.id) {
                 return event;
             }
         });
-        sport.events = events;
+    },
+
+    extendEventByBets: function(tabEvents, event) {
+        tabEvents.filter(function(tabEvent){
+            if(event.id == tabEvent.id) {
+                event.bets = tabEvent.bets
+            }
+        });
+    },
+
+    dataByPeriodAndSportName: function(period, sportName) {
+
+        var tabEvents = this.getTabEvents(period);
+        var sport = this.getSport(sportName);
+        var events = this.getEventsBySport(sport);
+        var self = this;
+        var eventsWithBet = events.map(function(event){
+            self.extendEventByBets(tabEvents, event);
+            return event;
+        });
+        sport.events = eventsWithBet;
         return {'sports': [sport]};
     }
 
