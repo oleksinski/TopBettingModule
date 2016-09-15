26b9d3d71ecd3b3dd6cfc0a117133666147c4ef8
romangl <rgluschuk@gmail.com>
2016-09-15 17:04:32 +0300

    group by time and sport name; add events

--- utils.js
+++ utils.js
@@ -1,11 +1,20 @@
 function Utils() {
 
-    this.dataByPeriodAndSportName = function(data, period, sport) {
+    this.dataByPeriodAndSportName = function(data, period, sportName) {
        var period = period;
-        var sport = sport;
+        var sport = data.sports.filter(function(sport){
+            if(sport.alias == sportName) {
+                return sport;
+            }
+        })[0];
+        var events = data.events.filter(function(event){
+            if(event.sportId == sport.id) {
+                return event;
+            }
+        });
+        sport.events = events;
+        return {'sports': [sport]};
     }
-
 };
-
 
 exports.utils = new Utils();
